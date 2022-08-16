b777a412fb0a5b53762c31843e9ad656441dcccf
N.Zotov <nikolai.zotov1@simbirsoft.com>
2022-08-16 09:41:20 +0400

    add steps

--- src/test/java/com/simbirsoft/tests/DemowebshopApiMethodTests.java
+++ src/test/java/com/simbirsoft/tests/DemowebshopApiMethodTests.java
@@ -2,6 +2,7 @@ package com.simbirsoft.tests;
 
 import com.simbirsoft.data.AddWishApiData;
 import com.simbirsoft.data.CheckWishApiData;
+import io.qameta.allure.Step;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static com.simbirsoft.filters.CustomLogFilter.customLogFilter;
 import static com.simbirsoft.page.ReqresApiMethods.checkValue;
 import static com.simbirsoft.page.ReqresApiMethods.getQuantity;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,26 +22,28 @@ public class DemowebshopApiMethodTests extends TestBase {
 
     @Test
     void addingProductToTheCart() {
-        AddWishApiData data = new AddWishApiData();
+        step("Добавляем товар", () -> {
+            AddWishApiData data = new AddWishApiData();
 
-        Response response = given()
-                .filter(customLogFilter().withCustomTemplates())
-                .contentType(data.contentType)
-                .body(data.body)
-                .log().uri()
-                .log().body()
-                .when()
-                .post(data.url)
-                .then()
-                .log().body()
-                .body(matchesJsonSchemaInClasspath("shemas/GenerateSheme.json"))
-                .statusCode(data.statusCode)
-                .extract()
-                .response();
+            Response response = given()
+                    .filter(customLogFilter().withCustomTemplates())
+                    .contentType(data.contentType)
+                    .body(data.body)
+                    .log().uri()
+                    .log().body()
+                    .when()
+                    .post(data.url)
+                    .then()
+                    .log().body()
+                    .body(matchesJsonSchemaInClasspath("shemas/GenerateSheme.json"))
+                    .statusCode(data.statusCode)
+                    .extract()
+                    .response();
 
-        assertThat(response.path("success").toString()).isGreaterThanOrEqualTo(data.textSuccessBuy);
-        assertThat(response.path("message").toString()).isGreaterThanOrEqualTo(data.textAnswer);
-        assertThat(response.path("updatetopcartsectionhtml").toString()).isGreaterThanOrEqualTo(data.textCountItem);
+            assertThat(response.path("success").toString()).isGreaterThanOrEqualTo(data.textSuccessBuy);
+            assertThat(response.path("message").toString()).isGreaterThanOrEqualTo(data.textAnswer);
+            assertThat(response.path("updatetopcartsectionhtml").toString()).isGreaterThanOrEqualTo(data.textCountItem);
+        });
     }
 
     @CsvSource({
@@ -48,6 +52,7 @@ public class DemowebshopApiMethodTests extends TestBase {
     })
     @ParameterizedTest(name = "{index} - {0} is a addingMultipleProductsToTheCart")
     void addingMultipleProductsToTheCart(int amountItem, int amountTest) {
+        step("Количество товара увеличивается, после каждого теста, в корзине", () -> {
         AddWishApiData data = new AddWishApiData();
         CheckWishApiData data1 = new CheckWishApiData();
         int numberOfCycles = 0;
@@ -76,11 +81,12 @@ public class DemowebshopApiMethodTests extends TestBase {
                 .extract()
                 .response();
 
-        assertThat(getQuantity(response2.getBody().print())).isEqualTo(amountItem);
+        assertThat(getQuantity(response2.getBody().print())).isEqualTo(amountItem);});
     }
 
     @Test
     void checkingTheShoppingCart() {
+        step("Проверяем ,что в коризине нету товара", () -> {
         CheckWishApiData data1 = new CheckWishApiData();
 
         Response response1 = given()
@@ -94,6 +100,6 @@ public class DemowebshopApiMethodTests extends TestBase {
                 .extract()
                 .response();
 
-        assertThat(checkValue(response1.getBody().print())).isEqualTo(data1.availabilityOfTheProductInTheCart);
+        assertThat(checkValue(response1.getBody().print())).isEqualTo(data1.availabilityOfTheProductInTheCart);});
     }
 }
