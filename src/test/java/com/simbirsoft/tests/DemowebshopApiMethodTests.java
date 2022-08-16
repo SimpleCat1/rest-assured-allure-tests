47492aa7ee2f1ded7221b63a64dd9c2170d4334b
N.Zotov <nikolai.zotov1@simbirsoft.com>
2022-08-16 09:57:07 +0400

    add allure

--- src/test/java/com/simbirsoft/tests/DemowebshopApiMethodTests.java
+++ src/test/java/com/simbirsoft/tests/DemowebshopApiMethodTests.java
@@ -2,9 +2,18 @@ package com.simbirsoft.tests;
 
 import com.simbirsoft.data.AddWishApiData;
 import com.simbirsoft.data.CheckWishApiData;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -19,7 +28,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class DemowebshopApiMethodTests extends TestBase {
 
-
+    @Epic("Неавторизованный пользователь")
+    @Feature("Добавление товара")
+    @Story("Добавление товара")
+    @Owner("SimplePerson")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Добавление товара")
+    @Description("Проверяем api добавления товара")
     @Test
     void addingProductToTheCart() {
         step("Добавляем товар", () -> {
@@ -46,6 +61,13 @@ public class DemowebshopApiMethodTests extends TestBase {
         });
     }
 
+    @Epic("Авторизованный пользователь")
+    @Feature("Добавление товара у одного пользователя")
+    @Story("Добавление товара")
+    @Owner("SimplePerson")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Добавление товара по кукам")
+    @Description("Проверяем api добавления товара и их увеличение от при каждом тесте")
     @CsvSource({
             "1, 1",
             "3, 2",
@@ -84,6 +106,13 @@ public class DemowebshopApiMethodTests extends TestBase {
         assertThat(getQuantity(response2.getBody().print())).isEqualTo(amountItem);});
     }
 
+    @Epic("Неавторизованный пользователь")
+    @Feature("Проверка коризны")
+    @Story("Корзина")
+    @Owner("SimplePerson")
+    @Severity(SeverityLevel.CRITICAL)
+    @DisplayName("Проверка корзины")
+    @Description("Проверяем api корзины")
     @Test
     void checkingTheShoppingCart() {
         step("Проверяем ,что в коризине нету товара", () -> {
